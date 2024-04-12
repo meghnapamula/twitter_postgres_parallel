3ada818d1749755a82d9f1ea0b1d9383f644705b
Meghna Pamula <mpamula123@gmail.com>
2024-04-11 21:16:09 -0700

    commit 3

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -12,8 +12,10 @@ echo '==========================================================================
 echo 'load pg_normalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time echo "$files" | parallel ./load_normalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time echo "$files" | parallel ./load_normalized_batch.sh
